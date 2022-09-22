90799f910c7a2652ea4727b1e63f3779b0edd6ab
Erdogan Sonmez <sonmezerdogan10@gmail.com>
2022-09-22 01:14:12 -0500

    Exercise5_13 added

---
+++ chapters/chapter_05/Exercise5_12.java
@@ -0,0 +1,16 @@
+package chapters.chapter_05;
+
+public class Exercise5_12 {
+
+    public static void main(String[]args){
+
+        double k=Math.pow(12000,0.5);
+        int i=0;
+        while(i<k){
+            i++;
+
+        }
+        System.out.println(i);
+
+    }
+}
